9bbbd8ab65fb653e49de4f8e910bde6765eba7b7
Matt Miller <thisismattmiller@gmail.com>
2015-08-13 16:16:43 -0400

    db

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+
+var db = require('./lib/db.js');
+
+
+db.createTriplesTable();
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -8,6 +15,8 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+
 
 app.get('/', function(request, response) {
   response.render('pages/index');
