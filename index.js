964976e3f9f27142ec0d17667555118e99348946
Matt Miller <thisismattmiller@gmail.com>
2015-08-15 16:49:50 -0400

    db tests

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ var app = express();
 
 
 var db = require('./lib/db.js');
+var rdf = require('./lib/rdf.js');
+
 
 //always populate the table if it has to be created
 db.createTriplesTable(function(err,created){
+
 	if (created) db.populateBaseData();
 });
+
+
+db.returnClassmark('zzh',function(err,data,relatedData){
+
+	rdf.rows2rdf(data,'jsonld')
+
+
+})
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -23,6 +34,27 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+
+//send to the /about page
+app.get('/classmark/:classmark', function(request, response) { 
+
+	console.log(request.headers.accept)
+
+	if (request.accepts('nt')) {
+
+	}else{
+		response.redirect(303,'/classmark/'+request.params.classmark+'/about');
+	}
+
+
+});
+
+
+
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ test/db.test.js
@@ -0,0 +1,128 @@
+var should = require('should')
+var db = require("../lib/db.js");
+
+
+
+
+describe('DB', function () {
+
+	this.timeout(10000)
+
+
+	before(function(done) {	
+		//drop the triple table and set the path to test data before we start
+		db.billingsLoadDataZip               = __dirname + '/../data/billings.test.json.zip';
+		db.billingsLoadDataZipOutput         = __dirname + '/../data/billings.test.json';
+		db.billingsLoadDataZipOutputFilename = __dirname + '/../data/billings.test.json/billings.test.json';
+
+		db.dropTriplesTable(function(err){
+			if (err){
+				if (err.code!='42P01') throw err				
+			}	
+			done()		
+		})
+
+	})
+
+
+	after(function(done) { 
+		db.dropTriplesTable(function(err){
+			if (err){
+				if (err.code!='42p01') throw err				
+			}
+			done()
+		})
+	});
+
+
+
+	it('It should build the triples table', function (done) {
+		db.createTriplesTable(function(err,results){
+			results.should.equal(true);
+			db.testTriplesTable(function(err,results){				
+				if (err) throw err;
+				results.rowCount.should.equal(0);
+				done()
+			})
+
+		})
+		
+	})
+
+
+	it('It should populate the test table', function (done) {
+		db.populateBaseData(function(err,results){
+			if (err) throw err;
+
+			//the counter from the method
+			results.should.equal(83)	
+
+			db.countTriplesTable(function(err,results){		
+	
+				if (err) throw err;
+				//the acutal database
+				results.rows[0].count.should.equal('83')
+				done()
+			})
+
+		})
+		
+	})
+
+	it('It should return data specifc to a requested classmark - classmarkData', function (done) {
+
+		db.returnClassmark('GIV',function(err,classmarkData,relatedMarksData){
+			classmarkData.length.should.equal(14);
+			classmarkData[0].subject.should.equal('class:giv');
+			classmarkData[0].predicate.should.equal('skos:hiddenLabel');
+			should.not.exist(classmarkData[0].objecturi);
+			classmarkData[0].objectliteral.should.equal('GIV');
+			classmarkData[0].literaldatatype.should.equal('@en');
+			classmarkData[0].provenance['@context'].should.equal('http://www.w3.org/2004/02/skos/core#');
+			done()
+		})
+	});
+
+
+	it('It should return data specifc to a requested classmark - relatedMarksData', function (done) {
+		db.returnClassmark('GIV',function(err,classmarkData,relatedMarksData){
+			relatedMarksData.length.should.equal(25);
+			done()
+		})
+	});
+	
+
+	it('It should return no resuts when a classmark is not found', function (done) {
+		db.returnClassmark('NOTINHERERERE',function(err,classmarkData,relatedMarksData){
+			classmarkData.should.equal(false);
+			relatedMarksData.should.equal(false);
+			done()
+		})
+	});
+
+	it('It should return no resuts when a classmark bad', function (done) {
+		db.returnClassmark(undefined,function(err,classmarkData,relatedMarksData){
+			classmarkData.should.equal(false);
+			relatedMarksData.should.equal(false);
+			db.returnClassmark(false,function(err,classmarkData,relatedMarksData){
+				classmarkData.should.equal(false);
+				relatedMarksData.should.equal(false);
+				db.returnClassmark(null,function(err,classmarkData,relatedMarksData){
+					classmarkData.should.equal(false);
+					relatedMarksData.should.equal(false);
+					db.returnClassmark(67890,function(err,classmarkData,relatedMarksData){
+						classmarkData.should.equal(false);
+						relatedMarksData.should.equal(false);
+						done()
+					})
+				})
+			})
+		})
+	});
+
+
+
+
+
+
+})
